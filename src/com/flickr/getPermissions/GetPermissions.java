e560f6b296cbe45f66fac88827b50ec006abc174
romilb <romilbansal@gmail.com>
2013-07-14 01:44:53 +0530

    Update Servlets

--- src/com/flickr/getPermissions/GetPermissions.java
+++ src/com/flickr/getPermissions/GetPermissions.java
@@ -1,7 +1,11 @@
 package com.flickr.getPermissions;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,20 +13,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.scribe.builder.ServiceBuilder;
-import org.scribe.builder.api.FlickrApi;
-import org.scribe.model.OAuthRequest;
 import org.scribe.model.Token;
-import org.scribe.model.Verb;
 import org.scribe.model.Verifier;
-import org.scribe.oauth.OAuthService;
 
 import com.flickr4java.flickr.Flickr;
 import com.flickr4java.flickr.FlickrException;
 import com.flickr4java.flickr.REST;
-import com.flickr4java.flickr.Response;
 import com.flickr4java.flickr.auth.Auth;
 import com.flickr4java.flickr.auth.AuthInterface;
+import com.flickr4java.flickr.photos.Photo;
+import com.flickr4java.flickr.photos.PhotoList;
+import com.flickr4java.flickr.photosets.Photoset;
+import com.flickr4java.flickr.photosets.Photosets;
+import com.flickr4java.flickr.photosets.PhotosetsInterface;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 /**
  * Servlet implementation class GetPermissions
@@ -30,89 +35,95 @@ import com.flickr4java.flickr.auth.AuthInterface;
 @WebServlet("/GetPermissions")
 public class GetPermissions extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public GetPermissions() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public GetPermissions() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @throws IOException 
 	 * @throws FlickrException 
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-    
-    public static void main(String[] args)
-    {
-    	String tok = "72157634624508602-b0e29aa8979329f3";
-		String verifier = "59b3db714dc28442";
-		 Verifier v = new Verifier(verifier);
-		 OAuthService service = new ServiceBuilder().provider(FlickrApi.class).apiKey("5f0a1d8f31426f811498e2ec5295c705").apiSecret("86bbe3f2c143379c").build();
-		 
-		 Token token = new Token(tok, "86bbe3f2c143379c");
-		 Token accessToken = service.getAccessToken(token, v);
 
-			// This token can be used until the user revokes it.
-			System.out.println("Token: " + accessToken.getToken());
-			System.exit(0);
-    	
-    	/*
-      // Replace these with your own api key and secret
-      String apiKey = "5f0a1d8f31426f811498e2ec5295c705";
-      String apiSecret = "86bbe3f2c143379c";
-      OAuthService service = new ServiceBuilder().provider(FlickrApi.class).apiKey(apiKey).apiSecret(apiSecret).build();
-      Scanner in = new Scanner(System.in);
+	public static void main(String[] args) throws IOException, FlickrException
+	{
 
-      System.out.println("=== Flickr's OAuth Workflow ===");
-      System.out.println();
-
-      // Obtain the Request Token
-      System.out.println("Fetching the Request Token...");
-      Token requestToken = service.getRequestToken();
-      System.out.println("Got the Request Token!");
-      System.out.println();
-
-      System.out.println("Now go and authorize Scribe here:");
-      String authorizationUrl = service.getAuthorizationUrl(requestToken);
-      System.out.println(authorizationUrl + "&perms=read");
-      System.out.println("And paste the verifier here");
-      System.out.print(">>");
-      Verifier verifier = new Verifier(in.nextLine());
-      System.out.println();
-
-      // Trade the Request Token and Verfier for the Access Token
-      System.out.println("Trading the Request Token for an Access Token...");
-      Token accessToken = service.getAccessToken(requestToken, verifier);
-      System.out.println("Got the Access Token!");
-      System.out.println("(if your curious it looks like this: " + accessToken + " )");
-      System.out.println();*/
-
-     }
-    
-    public static void main(String args) throws FlickrException{
-    	String tok = "72157634624508602-b0e29aa8979329f3";
-		String verifier = "59b3db714dc28442";
-		 Verifier v = new Verifier(verifier);
-		 OAuthService service = new ServiceBuilder().provider(FlickrApi.class).apiKey("5f0a1d8f31426f811498e2ec5295c705").apiSecret("86bbe3f2c143379c").build();
-		 
-		 Token token = new Token(tok, "86bbe3f2c143379c");
-		 Token accessToken = service.getAccessToken(token, v);
-
-			// This token can be used until the user revokes it.
-			System.out.println("Token: " + accessToken.getToken());
-
-    }
+	}
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String s = request.getParameter("oauth_verifier");
-		String tok = request.getParameter("oauth_token");
-		Flickr flickr = new Flickr("5f0a1d8f31426f811498e2ec5295c705", "86bbe3f2c143379c", new REST());
-		AuthInterface authInterface = flickr.getAuthInterface();
-		Token token = authInterface.getRequestToken();
-		Token requestToken = authInterface.getAccessToken(token, new Verifier(tok));
+		
+		File f = new File("/home/romil/temp.txt");
 
+		FileReader fi = new FileReader(f);
+
+		BufferedReader bw = new BufferedReader(fi);
+
+		String toke = bw.readLine();
+		String secret = bw.readLine();
+		Flickr flickr = new Flickr("5f0a1d8f31426f811498e2ec5295c705", "86bbe3f2c143379c", new REST());
+		Flickr.debugStream = false;
+		AuthInterface authInterface = flickr.getAuthInterface();
+		Token token = new Token(toke, secret);
+		String verifier = request.getParameter("oauth_verifier");
+
+		Verifier v = new Verifier(verifier);
+
+		Token accessToken = authInterface.getAccessToken(token, v);
+		//System.out.println("Authentication success");
+
+		try {
+			Auth auth = authInterface.checkToken(accessToken);
+			flickr.setAuth(auth);
+			String user = auth.getUser().getRealName();
+			PhotosetsInterface iface = flickr.getPhotosetsInterface();
+			Photosets photosets = iface.getList(auth.getUser().getId());
+			ArrayList<Photoset> p = (ArrayList<Photoset>) photosets.getPhotosets();
+			HashSet<String> set = new HashSet<String>();
+			ArrayList<String> photo = new ArrayList<String>();
+			Photoset curr = null;
+			for(Photoset pp : p){
+				set.add(pp.getTitle().split("->")[0]);
+				if(pp.getTitle().split("->").length==1){
+					curr=pp;
+				}
+			}
+			if(set.size()>0){
+				PhotoList<Photo> photos = iface.getPhotos(curr.getId(), 500, 1);
+				for(Photo ph:photos){
+					photo.add(ph.getLargeUrl());
+				}
+			}
+			else{
+
+			}
+			JsonObject jo = new JsonObject();
+			jo.addProperty("user", user);
+			jo.addProperty("id", auth.getUser().getId());
+			JsonArray ja = new JsonArray();
+			for(String s : set){
+				JsonObject j = new JsonObject();
+				j.addProperty("Name", s);
+				j.addProperty("Next", "home->"+s);
+				ja.add(j);
+			}
+			jo.add("folders", ja);
+			JsonArray ja2 = new JsonArray();
+			for(String s : photo){
+				JsonObject j = new JsonObject();
+				j.addProperty("Name", s);
+				ja.add(j);
+			}
+			jo.add("photos", ja2);
+			response.sendRedirect("http://localhost:8080/userimage.html?j="+jo.toString());
+		} catch (FlickrException e) {
+			// TODO Auto-geneated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
--- src/com/flickr/getPermissions/requestPermissions.java
+++ src/com/flickr/getPermissions/requestPermissions.java
@@ -1,5 +1,8 @@
 package com.flickr.getPermissions;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -34,6 +37,19 @@ public class requestPermissions extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    public static void main(String arg[]){
+    	Flickr flickr = new Flickr("5f0a1d8f31426f811498e2ec5295c705", "86bbe3f2c143379c", new REST());
+		Flickr.debugStream = false;
+		AuthInterface authInterface = flickr.getAuthInterface();
+
+		//Scanner scanner = new Scanner(System.in);
+
+		Token token = authInterface.getRequestToken("http://www.localhost:8080/getPermissions");
+		System.out.println("token: " + token.getToken() + " "+token.getSecret());
+
+		String url = authInterface.getAuthorizationUrl(token, Permission.DELETE);
+    }
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		Flickr flickr = new Flickr("5f0a1d8f31426f811498e2ec5295c705", "86bbe3f2c143379c", new REST());
@@ -42,13 +58,24 @@ public class requestPermissions extends HttpServlet {
 
 		//Scanner scanner = new Scanner(System.in);
 
-		Token token = authInterface.getRequestToken("http://www.localhost:8080/getPermissions");
-		//System.out.println("token: " + token);
+		Token token = authInterface.getRequestToken("http://localhost:8080/FlickrDemo/getPermissions");
+		System.out.println("token: " + token.getToken() + " "+token.getSecret());
 
 		String url = authInterface.getAuthorizationUrl(token, Permission.DELETE);
+		
+		File f = new File("/home/romil/temp.txt");
+		
+		FileWriter fi = new FileWriter(f);
+		
+		BufferedWriter bw = new BufferedWriter(fi);
+		
+		bw.append(token.getToken()+"\n");
+		bw.append(token.getSecret());
+		bw.flush();
+		bw.close();
+		System.out.println(url);
 		response.sendRedirect(url);
 	}
-	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
