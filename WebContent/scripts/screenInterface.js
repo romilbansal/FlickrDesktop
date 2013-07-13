955c37fbd061962017b0a4da1a28832135caed84
romilb <romilbansal@gmail.com>
2013-07-13 21:11:10 +0530

    initial commit

---
+++ WebContent/scripts/screenInterface.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+  var tempI = 0;
+  var remove = true;
+  $(".testDiv").click(function(){
+    // only for text
+    $(this).hide();
+  });
+  
+    $(".thumbImage").click(function(e){
+    
+        var xCoord = e.pageX;
+        var yCoord = e.pageY;
+    
+        var content = "<div class='thumbMenu' id='tempMenu'>HELLOOOO</div>";
+        $(this).after(content);
+    
+        $("#tempMenu").css("visibility","visible");
+        $("#tempMenu").css("left",xCoord-30);
+        $("#tempMenu").css("top",yCoord-30);
+        return false;
+      
+    });
+
+    $(".thumbImage").mouseout(function(e){
+        
+        $("#tempMenu").css("background-color","gray");
+        return false;
+      
+    });
+    
+    
+    $(".thumbMenu").mouseout(function(e){
+        alert("Hi");
+        
+    });
+    
+});
---
+++ src/com/flickr/getPermissions/requestPermissions.java
@@ -0,0 +1,59 @@
+package com.flickr.getPermissions;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.scribe.model.Token;
+
+import com.flickr4java.flickr.Flickr;
+import com.flickr4java.flickr.REST;
+import com.flickr4java.flickr.auth.AuthInterface;
+import com.flickr4java.flickr.auth.Permission;
+
+/**
+ * Servlet implementation class requestPermissions
+ */
+@WebServlet("/requestPermissions")
+public class requestPermissions extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public requestPermissions() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Flickr flickr = new Flickr("5f0a1d8f31426f811498e2ec5295c705", "86bbe3f2c143379c", new REST());
+		Flickr.debugStream = false;
+		AuthInterface authInterface = flickr.getAuthInterface();
+
+		//Scanner scanner = new Scanner(System.in);
+
+		Token token = authInterface.getRequestToken("localhost:8080/index2.html");
+		//System.out.println("token: " + token);
+
+		String url = authInterface.getAuthorizationUrl(token, Permission.DELETE);
+		response.sendRedirect(url);
+	}
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
